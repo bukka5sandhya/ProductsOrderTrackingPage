db8986bfad98cc03028ea285ded7825a5d9ac1ff
bukka5sandhya <133884532+bukka5sandhya@users.noreply.github.com>
2024-02-19 15:52:38 +0530

    Update Routes.jsx

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -8,9 +8,8 @@ const ProjectRoutes = () => {
     <React.Suspense fallback={<>Loading...</>}>
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="*" element={<NotFound />} />
-          <Route path="/frameone" element={<FrameOne />} />
+         
+          <Route path="/" element={<FrameOne />} />
         </Routes>
       </Router>
     </React.Suspense>
