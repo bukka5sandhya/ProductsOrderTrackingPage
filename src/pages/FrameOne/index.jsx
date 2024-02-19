c6848f6d6028698f4ad464b44cadcf2c9dd3e1a1
bukka5sandhya <bukkapatansandhya920@gmail.com>
2024-02-19 15:48:00 +0530

    first commit

---
+++ src/pages/FrameOne/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import { Img } from "components";
+
+const FrameOnePage = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center justify-start mx-auto w-full">
+        <Img
+          className="h-[1703px] sm:h-auto object-cover w-full"
+          src="images/img_imgonlinecomu.png"
+          alt="imgonlinecomu"
+        />
+      </div>
+    </>
+  );
+};
+
+export default FrameOnePage;
